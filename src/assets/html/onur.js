7a86bc6a546f618e8b1eedd8b20a2428319a3b37
homur <onuratalay@gmail.com>
2021-09-07 14:10:46 +0200

    removing unused files, persisting sass-loader issue

--- src/assets/html/onur.js
+++
@@ -1,7 +0,0 @@
-export default html = 
-`
-<div>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti similique expedita dolore qui recusandae? Voluptate explicabo sed tenetur nulla doloribus est a fugit quasi repellendus fugiat, excepturi quae aspernatur praesentium expedita dolores, illo tempora ipsum perferendis iure eveniet possimus vel aperiam. Natus eos praesentium quidem expedita placeat inventore qui hic.
-</div>
-`
-
--- src/utils/getData.js
+++
@@ -1,7 +0,0 @@
-
-const getData = async (name) => {
-    const data = require('../data/' + name + '.json');
-    return data;
-}
-
-export default getData;
